89fba7672876d35f7b4623ef5227939569d2a060
MobinaMokhbery <mobinamokhbery@gmail.com>
2025-07-29 20:30:32 +0330

    mobina added file

--- src/mft/MobinaMokhbery.java
+++ src/mft/MobinaMokhbery.java
@@ -2,6 +2,6 @@ package mft;
 
 public class MobinaMokhbery {
     public static void main(String[] args) {
-        System.out.println("Mobina Mokhbery");
+        System.out.println("MobinaMokhbery");
     }
 }
